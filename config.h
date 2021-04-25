8b428aff99818faca556bc72e6b19a75173df08c
reqsi <pettid412@gmail.com>
2021-04-25 03:39:47 +0000

    Add new file

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "",  "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,10 +58,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL }; 
+static const char *scratchpadcmd[] = { "urxvt", "-t", scratchpadname, "-g", "120x34", NULL }; 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
